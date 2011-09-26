7fac3880ca3c11d896e82ddac3c4e24493798196
J. Mark Locklear <marklocklear@gmail.com>
2011-09-26 15:43:29 -0400

    first commit

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,79 @@
+class QuestionsController < ApplicationController
+  # GET /questions
+  # GET /questions.xml
+  def index
+    @questions = Question.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @questions }
+    end
+  end
+
+  # GET /questions/1
+  # GET /questions/1.xml
+  def show
+    @question = Question.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @question }
+    end
+  end
+
+  # GET /questions/new
+  # GET /questions/new.xml
+  def new
+    @questions = Array.new(3) { Question.new }
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @question }
+    end
+  end
+
+  # GET /questions/1/edit
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  # POST /questions
+  # POST /questions.xml
+  def create
+    @questions = params[:questions].values.collect { |question| Question.new(question) }
+    if @questions.all?(&:valid?)
+      @questions.each(&:save!)
+      redirect_to :action => 'index'
+    else
+      render :action => 'new'
+    end
+  end
+
+  # PUT /questions/1
+  # PUT /questions/1.xml
+  def update
+    @question = Question.find(params[:id])
+
+    respond_to do |format|
+      if @question.update_attributes(params[:question])
+        format.html { redirect_to(@question, :notice => 'Question was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @question.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /questions/1
+  # DELETE /questions/1.xml
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(questions_url) }
+      format.xml  { head :ok }
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MultipleModelsOneForm::Application.config.secret_token = 'b3f199e23800621db6df71048789b939d1a42d1fb0239f64d8fd448904dadf80e9d54db2d807a8267c64d984add4998c1b83903769ba6347d8b4b04343409b45'
